c33837476f8d7a129be91e01acad91593a9b868a
Matthew Pease <48925742+peaseboy2234@users.noreply.github.com>
2022-04-27 15:13:35 +0000

    more responses

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ print ('\033[91m' '\033[1m'"Enjoy the game folks"'\033[0m' '\033[96m')
 
 import random
 
-n = random.randint(1, 99)
-guess = int(input("Enter a number from 1 to 99: "))
+n = random.randint(1, 100)
+guess = int(input("Enter a number from 1 to 100: "))
 while n != "guess":
   print
   if guess < n:
     print ("guess is low")
-    guess = int(input("Enter an number from 1 to 99: "))
+    guess = int(input("Enter an number from 1 to 100: "))
   elif guess > n:
     print ("guess is high")
-    guess = int(input("Enter an number from 1 to 99: "))
+    guess = int(input("Enter an number from 1 to 100: "))
   else:
     print ("CONGRATS! You got it right, do you want a cookie?")
     
@@ -36,3 +36,13 @@ if choice == "No":
   
 if choice == "NO":
   print ("Stop yelling, my gosh")  
+
+if choice == "yah":
+  print ("No")
+
+if choice == "Yah":
+  print ("I guess you can have one *gives cookie*")
+
+if choice == "No thank you":
+  print ("*gives cookie* thanks for being nice")
+
